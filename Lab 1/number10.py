1698a46ae878099c02be5092823ba76215552343
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-07-25 21:39:50 -0400

    Create number10.py

---
+++ Lab 1/number10.py
@@ -0,0 +1,4 @@
+# number10
+#Jupyter notebook is an open source software that allows you to share documents on the web that contains code, equations etc.
+# Jupyter notebook is a development environment
+# Some alternatives to Jupter notebook are VSCode, nteract, & Jupyterlyte
