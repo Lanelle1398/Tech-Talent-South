5bd1f50e9a9d01567a3e8ad0c87373bbb415a8e7
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-07-25 21:32:09 -0400

    Create number4.py

---
+++ Lab 1/number4.py
@@ -0,0 +1,8 @@
+#What is the output of the following Python code
+# x = 10
+# y = 50
+# if (x ** 2 > 100 and y < 100): ** is an asterik
+#     print(x, y)
+
+
+# The code does not exeute through the loop because x**2 is equal to 100 not more
