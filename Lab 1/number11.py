b5581dd07263be0852036cadbe90726520a35477
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-07-25 22:14:48 -0400

    Create number11.py

---
+++ Lab 1/number11.py
@@ -0,0 +1,5 @@
+#Write a for loop that multiplies the number 4 by all the integers from 0 to 10 and prints the following for each: "4 multiplied by [i] is [x]"
+
+for x in range(11):
+    y=4*x
+    print( " 4 multiplied by " + str(x) + " is "+ str(y))
