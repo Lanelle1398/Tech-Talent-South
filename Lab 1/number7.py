2b5c1c8e7f0f97fb58a2946337541fb69c66ddc6
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-07-25 21:37:47 -0400

    Create number7.py

---
+++ Lab 1/number7.py
@@ -0,0 +1,4 @@
+#number 7
+# What is the output of the following code and what arithmetic operators are used here? print(2 * 3 ** 3 * 4)
+# these are the multiplication(*) and the power (*) operations
+# the answer is 216: 2 * 27*4
