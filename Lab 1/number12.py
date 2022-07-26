6a63d8fb569e618f5047605a652469bb5b42d658
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-07-25 22:15:11 -0400

    Create number12.py

---
+++ Lab 1/number12.py
@@ -0,0 +1,9 @@
+##number 12 -multiplication table
+for x in range(1,11):
+    for y in range(1,11):
+        print(x*y, end='\t')
+    print('')
+        
+        #end allows the numbers to stay on the same line
+        #\t\ allows the user to move to a new line
+        #print('') inserts a new lien at each set of 10
