4b050f686de5a0058858bd41e0a64aa693f42645
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-07-31 16:50:49 -0400

    Update number 6.py

--- Lab 1/number 6.py
+++ Lab 1/number 6.py
@@ -1,4 +1,4 @@
 #number 6
 # What is the output of the following code and what arithmetic operators is being used here?
 # print(2%6)
-# this is the modulus operator and the answer is 0 because there is no numerator
+# this is the modulus operator and the answer is 0 because there is no remainder
