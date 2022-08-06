7be14144b81e9d5d486e50debfb52613876c4683
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-08-05 21:04:13 -0400

    Update number5.py

--- lab2/number5.py
+++ lab2/number5.py
@@ -1,3 +1,6 @@
+# Iterate through a given list and check if a given element already exists in a dictionary as a key’s value. If not, delete it from the list.
+# rollNumber = [47, 64, 69, 37, 76, 83, 95, 97]
+# sampleDict ={'Zach':47, 'Emma':69, 'Kelly':76, 'Jason':97}
 
 rollNumber = [47, 64, 69, 37, 76, 83, 95, 96, 97]
 sampleDict = {"Jhon": 47, "Emma": 69, "Kelly": 76, "Jason": 97}
