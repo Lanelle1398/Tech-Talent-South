b8f3c14c44ef5392c24ee458e8e1759830979a58
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-08-05 21:04:55 -0400

    Create number7.py

---
+++ lab2/number7.py
@@ -0,0 +1,6 @@
+import random
+guessedNumber="" #initialize guessedNumber
+randomNumber=random.randint(1, 10)
+while randomNumber!=guessedNumber:
+    guessedNumber=int(input("Guess a number between 1 and 10 until you get it right: "))
+print("you guessed correct!")
