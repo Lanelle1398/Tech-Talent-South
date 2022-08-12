c7a286b101060f3b5501c24cd2a51b7cbe00a7ff
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-08-11 22:04:00 -0400

    Create number5.py

---
+++ Lab 3(Mod 04-05)/number5.py
@@ -0,0 +1,6 @@
+##########combinations with replacement #####
+from itertools import combinations_with_replacement
+word , numberOfPerms = input(" enter word then number of permutations: \n").split()
+
+for i in combinations_with_replacement(sorted(word),int(numberOfPerms)): #given a word, and number of perms, find permutations, create a list, iterate over that list
+      print(*i,sep='')
