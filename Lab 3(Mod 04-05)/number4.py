1018fafb4a0e3b8675a5938947e035bbbe6cb14a
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-08-11 22:03:23 -0400

    Create number4.py

---
+++ Lab 3(Mod 04-05)/number4.py
@@ -0,0 +1,5 @@
+# from itertools import permutations
+# word , numberOfPerms = input(" enter word then number of permutations: \n").split()
+
+# for i in list(permutations(sorted(word),int(numberOfPerms))): #given a word, and number of perms, find permutations, create a list, iterate over that list
+#       print(*i,sep='')
