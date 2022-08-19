f839db482de5596973046dfcff45e74a00a5df17
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-08-18 20:42:49 -0400

    Update number1.py

--- Mod 6-7/number1.py
+++ Mod 6-7/number1.py
@@ -1,9 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 import re
 index=int(input()) #the user should enter 11 for 11 lines
 for i in range(0,index):
-   txt = input()
-   txt = re.sub(r"\ \&\&\ "," and ",txt)
-   txt = re.sub(r"\ \|\|\ "," or ",txt)
-#    txt = re.sub(r"\ \&\&\ "," and ",txt)
-#    txt = re.sub(r"\ \|\|\ "," or ",txt)
-   print(txt)
+    txt = input()
+    txt = re.sub(r"(?<= )(&&)(?= )", "and",txt, count=1000)
+    # negative and positive look behind.
+    # What comes before && must be  a space '&&' what comes after '&&' must also be a space
+    # replace the first thousand occurences
+    txt = re.sub(r"(?<= )(\|\|)(?= )","or",txt, count=1000)
+    print(txt)
+
