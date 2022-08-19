2338c113654d830f08350bdac55b5387ec47c814
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-08-18 21:10:16 -0400

    Update number2.py

--- Mod 6-7/number2.py
+++ Mod 6-7/number2.py
@@ -5,3 +5,14 @@ for i in range(index):
         txt=input() #get line1, then line 2 etc
         if bool(re.search("<[a-z](\w|-|_|\.)+@[a-z]+\.[a-z]{1,3}>", txt)): #if you find this pattern in the text, print out the entire string
             print(txt)
+       
+# import email.utils
+# import email.utils
+# import re
+# n = int(input())
+# for i in range(n):
+#     p = email.utils.parseaddr(input())
+#     if re.match(r'[a-z][\w.-]+@[a-z]+\.[a-z]{1,3}$', p[1], re.I):
+#         #print (formataddr(p))
+#         print(email.utils.formataddr(p))
+        
