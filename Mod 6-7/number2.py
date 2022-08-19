1a1332c3250b1ce22f685fe193d7d382fb4dc703
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-08-19 18:14:26 -0400

    Update number2.py

--- Mod 6-7/number2.py
+++ Mod 6-7/number2.py
@@ -15,4 +15,18 @@ for i in range(index):
 #     if re.match(r'[a-z][\w.-]+@[a-z]+\.[a-z]{1,3}$', p[1], re.I):
 #         #print (formataddr(p))
 #         print(email.utils.formataddr(p))
+//other methodd
+
+# import re
+# index=int(input()) #loop twice; user should enter 2
+# for i in range(index):
+#         txt=input() #get line1, then line 2 etc
+#         if bool(re.search("<[a-z](\w|-|_|\.)+@[a-z]+\.[a-z]{1,3}>", txt)):
+#             lst = re.findall('^([\w\-]+)', (txt))  
+#             lst2=re.findall(r'<[\w.+-]+@[\w-]+\.[\w.-]+>', txt)
+#             print(*(lst +lst2))
+#         # +
+        
+#         # Printing of List 
+#         #print(lst)
         
