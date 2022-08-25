177837720cc5d3c0e2a3331c5163e70b722cf32b
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-08-25 19:54:51 -0400

    Update number1.py

--- mod8 OOP and Inheritance/number1.py
+++ mod8 OOP and Inheritance/number1.py
@@ -8,16 +8,25 @@ class Person:
 
 class Student(Person):
       def desc(self):
-          #desc_person = "%s %s is a student." % (self.fname, self.lname)
            desc_person = "First name: %s \nLast name: %s \nStudent ID: %d" % (self.fname, self.lname, self.studentID)
            return desc_person
         
 class Grades(Student):
       def desc(self, myList):
-           #self.avg=statistics.mean(myList)
-           desc_person = self.fname2+ " " + self.lname2 + " " + "Student ID " + str(self.studentID2) + ", you got a " + str(statistics.mean(myList))
-           #"%s %s Student ID %d , you got a " % (self.fname2, self.lname2, self.studentID2)
-           return desc_person
+            averageVal=statistics.mean(myList)
+            if  90 <= averageVal <= 100:
+                x="A"
+            if  80 <= averageVal < 90:
+                x="B"
+            if  70 <=  averageVal < 80:
+                x="C"
+            if  60 <=  averageVal < 70:
+                x="D"
+            if   averageVal  < 50:
+                x="F"
+            desc_person = self.fname2+ " " + self.lname2 + " " + "Student ID " + str(self.studentID2) + ", you got a " + x
+      
+            return desc_person
             
 person2 = Student()
 person2.fname =input("Enter a first name: \n")
@@ -30,7 +39,6 @@ studentGrades.lname2=person2.lname
 studentGrades.studentID2=person2.studentID
 
 
-input_int_array= [int(x) for x in input("enter a list of numbers").split()]
+input_int_array= [int(x) for x in input("enter a list of space separated numbers ").split()]
 print(studentGrades.desc(input_int_array))
-
 
