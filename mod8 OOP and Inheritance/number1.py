dba290347d191fa93e022ebc8114d0603627ce39
Lanelle1398 <88471126+Lanelle1398@users.noreply.github.com>
2022-08-24 21:03:48 -0400

    Update number1.py

--- mod8 OOP and Inheritance/number1.py
+++ mod8 OOP and Inheritance/number1.py
@@ -1,4 +1,4 @@
-
+import statistics
 class Person:
     fname = ""
     lname = ""
@@ -13,16 +13,28 @@ class Student(Person):
            return desc_person
         
 class Grades(Student):
-      def desc(self):
-           desc_person = "First name: %s \n Last name %s" % (self.fname, self.lname)
+      def desc(self, myList):
+           #self.avg=statistics.mean(myList)
+           desc_person = self.fname2+ " " + self.lname2 + " " + "Student ID " + str(self.studentID2) + ", you got a " + str(statistics.mean(myList))
+           #"%s %s Student ID %d , you got a " % (self.fname2, self.lname2, self.studentID2)
            return desc_person
+            
 person2 = Student()
-
 person2.fname =input("Enter a first name: \n")
 person2.lname = input("Enter a last name: \n")
 person2.studentID=int(input("Enter a student ID number: \n"))
-print(person2.desc())
 
+studentGrades=Grades()
+studentGrades.fname2=person2.fname
+studentGrades.lname2=person2.lname
+studentGrades.studentID2=person2.studentID
+
+#print(person2.desc())
 # take multiple inputs in array
-input_int_array = [int(x) for x in input("enter a list of numbers").split()]
-print("array:", input_int_array)
+input_int_array= [int(x) for x in input("enter a list of numbers").split()]
+#print(studentGrades.desc(input_int_array))
+print(studentGrades.desc(input_int_array))
+
+#studentGrades.myList=input_int_array 
+#print("array:", input_int_array)
+
